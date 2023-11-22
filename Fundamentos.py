1fedef84505b16c0c123b961011924cdd80371ce
Roman Sanchez2 <roman.sanpad22@gmail.com>
2023-11-21 22:07:40 -0600

    primeros fundamentos

---
+++ Fundamentos.py
@@ -0,0 +1,7 @@
+#Escribir un programa que muestre por pantalla la cadena ¡Hola Mundo!
+print("¡Hola Mundo!")
+
+#Escribir un programa que almacene la cadena ¡Hola Mundo! en una variable y luego la muestre por pantalla el contenido de la variable
+var = "¡Hola Mundo!"
+
+print(var)  
