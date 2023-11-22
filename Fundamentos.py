535d871606090fa2fd8743b3d129555ce1e8e00d
Roman Sanchez2 <roman.sanpad22@gmail.com>
2023-11-21 22:31:40 -0600

    Ejercicio 4

--- Fundamentos.py
+++ Fundamentos.py
@@ -10,3 +10,6 @@ print(var)
 #donde <name> es el nombre del usuario ya introducido   
 name = input("Registra tu nombre: ")
 print(name)
+
+#Escribir un programa que muestre por pantalla el resultado de la siguiente operacion aritmetica [(3+2)/(2.5)]
+print((3+2)/(2*5)**2)
