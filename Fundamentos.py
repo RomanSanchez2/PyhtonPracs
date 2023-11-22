00a2cffc5c00505bb8ce1de304f0491eaa4bcb29
Roman Sanchez2 <roman.sanpad22@gmail.com>
2023-11-21 22:28:35 -0600

    ejercicio 3

--- Fundamentos.py
+++ Fundamentos.py
@@ -5,3 +5,8 @@ print("¡Hola Mundo!")
 var = "¡Hola Mundo!"
 
 print(var)  
+
+#Escribir un programa que pregunte el nombre del usuario en la consola y despues de que el usuario lo introduzca muestre por pantalla la cadena ¡Hola <name>!
+#donde <name> es el nombre del usuario ya introducido   
+name = input("Registra tu nombre: ")
+print(name)
